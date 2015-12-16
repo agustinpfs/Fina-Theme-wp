375f15b61f532725ab848fb6f1e2decf6752ba87
agustinpfs <agustinpfs@gmail.com>
2015-12-16 20:00:35 -0300

    remove head files

--- head.php
+++
@@ -1,9 +0,0 @@
-<header class="Header">
-	<nav class="Header-menu">
-		
-	</nav>
-	<div class="Header-title">
-		<h1 class="Header-h1">F</h1>
-		<h3><?php bloginfo('description' ); ?></h3>
-	</div>
-</header>
--- header.php
+++ header.php
@@ -8,5 +8,12 @@
 	</head>
 
 <body>
-	<?php include TEMPLATEPATH . '/head.php' ?>	
-
+	<header class="Header">
+		<nav class="Header-menu">
+			
+		</nav>
+		<div class="Header-title">
+			<h1 class="Header-h1">F</h1>
+			<h3><?php bloginfo('description' ); ?></h3>
+		</div>
+	</header>
