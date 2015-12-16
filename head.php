50f0b02bfb01776105e8581c913cdbb2f86e48a3
agustinpfs <agustinpfs@gmail.com>
2015-12-16 19:57:23 -0300

    add general.styl and remove test. Create head module

---
+++ head.php
@@ -0,0 +1,9 @@
+<header class="Header">
+	<nav class="Header-menu">
+		
+	</nav>
+	<div class="Header-title">
+		<h1 class="Header-h1">F</h1>
+		<h3><?php bloginfo('description' ); ?></h3>
+	</div>
+</header>
--- header.php
+++ header.php
@@ -5,5 +5,8 @@
 		<title><?php bloginfo('description'); ?></title>
 		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 		<link rel="stylesheet" href="<?php bloginfo(stylesheet_url ); ?>">
-		<title></title>
 	</head>
+
+<body>
+	<?php include TEMPLATEPATH . '/head.php' ?>	
+
