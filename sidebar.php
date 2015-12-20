02bd6b5888b58946b2290e0542b2754fbaabc24d
agustinpfs <agustinpfs@gmail.com>
2015-12-20 17:38:09 -0300

    refactor single & sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,17 +1,3 @@
-<section class="Sidebar">
-	<br><br>
-	<br>
-
-	<aside class="audio">
-	<?php rewind_posts(); ?>
-	<?php query_posts('cat=6'); ?>
-		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		<div class="">
-			<?php the_content(); ?>
-		
-		</div>
-		<?php endwhile; ?>
-		<?php endif; ?>	
-	</aside>
+<section class="Sidebar">	
 	<?php dynamic_sidebar('unique-sidebar-idt'); ?>
 </section>
--- single.php
+++ single.php
@@ -5,67 +5,60 @@
 	<link rel="stylesheet" href="<?php bloginfo(stylesheet_url ); ?>">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 </head>
-<main class="Single">
-<!-- <div class="Single-header">
-</div> -->
-	<section class="Single-post">
- 		<?php rewind_posts(); ?>
-		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-			<article class="Single-article">
-				<div class="Single-logo">
-					<figure>
-						<h1 class="Single-h1">F</h1>
-					</figure>
-					<h2><?php bloginfo('description' ); ?></h2>
-				</div>	
-			<?php include TEMPLATEPATH . '/contact.php' ?>
-				<header class="Single-header">
+<body class="Single">
+	<header class="Single-header">
+		<div class="Single-logo">
+			<figure>
+				<h1 class="Single-h1">F</h1>
+			</figure>
+			<h2><?php bloginfo('description' ); ?></h2>
+		</div>
+	</header>
+	<div class="Single-container">
+		<main class="Single-post">
+	 		<?php rewind_posts(); ?>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>				
+				<article class="Single-article">					
+					<?php include TEMPLATEPATH . '/contact.php' ?>
 					<h1 class="Single-title">						
-							<?php the_title(); ?>						
-					</h1>
-					<!-- <div class="Post-data">
-						<small><?php the_date(); ?></small>
-						<i>por <strong><?php the_author(); ?></strong></i>
-					</div> -->
-				</header>
-				
-				<div class="Single-content">
+						<?php the_title(); ?>						
+					</h1>				
 					<figure>
 						<?php the_post_thumbnail(large); ?>
 					</figure>
+					<div class="Single-content">
 				     <?php the_content(); ?>
-				</div>
-			
-				<div class="Post-data Post-data--footer  ">
-					<div class="Post-cat">
-	 					<span>Categoría</span>
-	 					<small><?php the_category(); ?></small>
- 					</div> 					
- 					<small><em><?php comments_number() ?></em></small>
-				</div>			
-			</article>
+					</div>			
+					<div class="Post-data Post-data--footer  ">
+						<div class="Post-cat">
+		 					<span>Categoría</span>
+		 					<small><?php the_category(); ?></small>
+	 					</div> 					
+					</div>	
+					<div class="Single-comments">
+						<?php comments_template(); ?>
+					</div>		
+				</article>
+				<?php endwhile; ?>
+				<!-- post navigation -->
+				<?php else: ?>
+					<h3>No se encontraron entradas</h3>
+				<!-- no posts found -->
+				<?php endif; ?>
 
-			
-			
-		<?php endwhile; ?>
-		<!-- post navigation -->
-		<?php else: ?>
-			<h3>No se encontraron entradas</h3>
-		<!-- no posts found -->
-		<?php endif; ?>
-		<div class="Single-comments">
-			<?php comments_template(); ?>
+	<!-- <div class="Post-data">
+	<small><?php the_date(); ?></small>
+	<i>por <strong><?php the_author(); ?></strong></i>
+	</div> -->			
+		</main>
+		<div class="Post-sidebar">
+			<?php include TEMPLATEPATH . '/sidebar.php' ?>
 		</div>
+	
+<!-- // <?php get_footer(); ?> -->
 
+</body>
 
-	</section>
-	<div class="Post-sidebar">
-		<?php include TEMPLATEPATH . '/sidebar.php' ?>
-	</div>
-
-
-</main>
-<?php get_footer(); ?>
 
 
 
