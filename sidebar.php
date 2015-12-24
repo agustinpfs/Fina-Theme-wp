14168016fafcf2bbf79bca85e2e4eb3dcd5d6f33
agustinpfs <agustinpfs@gmail.com>
2015-12-24 10:24:59 -0300

    right menu slide

--- sidebar.php
+++ sidebar.php
@@ -1,26 +1,36 @@
-<div class="Sidebar-slide">
-	<div class=""></div>
-</div>	
-<section class="Sidebar-content">
-	<?php include TEMPLATEPATH . '/slider-menu.php' ?>
-	<?php include TEMPLATEPATH . '/recent-posts.php' ?>
-	<?php dynamic_sidebar('unique-sidebar-idt'); ?>
-</section>
+
+	<div class="Sidebar-slide">
+		<div class=""></div>
+	</div>
+	<section class="Sidebar-content">
+		<?php include TEMPLATEPATH . '/slider-menu.php' ?>
+		<?php include TEMPLATEPATH . '/recent-posts.php' ?>
+		<?php include TEMPLATEPATH . '/recent-posts.php' ?>
+		<?php include TEMPLATEPATH . '/recent-posts.php' ?>
+		<?php include TEMPLATEPATH . '/recent-posts.php' ?>
+		<?php include TEMPLATEPATH . '/recent-posts.php' ?>
+		<?php include TEMPLATEPATH . '/recent-posts.php' ?>
+		<?php include TEMPLATEPATH . '/recent-posts.php' ?>
+		<?php include TEMPLATEPATH . '/recent-posts.php' ?>
+		<?php include TEMPLATEPATH . '/recent-posts.php' ?>
+		<?php dynamic_sidebar('unique-sidebar-idt'); ?>
+	</section>
 
 <script>
 	function showSlider() {
-	$( ".Sidebar-content" ).addClass('width3')
-	$( ".Single-post" ).addClass('respo-post')
+	$( ".Sidebar" ).toggleClass('slide-menu')
+	// $( ".Single-post" ).addClass('respo-post')
 
 	}
-	function hiddeSlider() {
-	$( ".Sidebar-content" ).removeClass('width3')
-	$( ".Single-post" ).removeClass('respo-post')
+	// function hiddeSlider() {
+	// $( ".Sidebar" ).removeClass('width3')
+	// $( ".Single-post" ).removeClass('respo-post')
 
-	}
-	// $( ".Sidebar-slide" ).hover(showSlider)
-	$( ".Sidebar-slide" ).mouseenter( showSlider ).mouseleave( hiddeSlider );
-	$( ".Sidebar-content" ).mouseenter( showSlider ).mouseleave( hiddeSlider );
+	// }
+	// $( ".Sidebar" ).on("click", showSlider)
+	// $( ".Sidebar" ).mouseenter( showSlider ).mouseleave( hiddeSlider );
+	$( ".Sidebar" ).mouseenter( showSlider ).mouseleave( showSlider );
+	// $( ".Sidebar-content" ).mouseenter( showSlider ).mouseleave( hiddeSlider );
 
 	// $( ".Header-icon" ).click(function() {
 	//   $( ".Header-nav" ).slideToggle( "slow" );
--- single.php
+++ single.php
@@ -5,60 +5,58 @@
 	<link rel="stylesheet" href="<?php bloginfo(stylesheet_url ); ?>">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 </head>
-<body class="Single">
+<body>
 	
-	<div class="Single-container">
-		<main class="Single-post">
-
-	 		<?php rewind_posts(); ?>
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>				
-				<article class="Single-article">
-					<header class="Single-header">
-						<div class="Single-logo">
-							<figure>
-								<h1 class="Single-h1">F</h1>
-							</figure>
-							<h2><?php bloginfo('description' ); ?></h2>
-						</div>
-					</header>					
-					<?php include TEMPLATEPATH . '/contact.php' ?>
+	<main class="Single">
+		<header class="Single-header">
+			<div class="Single-logo">
+				<figure>
+					<h1 class="Single-h1">F</h1>
+				</figure>
+				<h2><?php bloginfo('description' ); ?></h2>
+			</div>
+		</header>
+ 		<?php rewind_posts(); ?>
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>				
+		<article class="Single-article">
+								
+<!-- 			// <?php include TEMPLATEPATH . '/contact.php' ?>
+ -->			
+			<h1 class="Single-title">						
+				<?php the_title(); ?>						
+			</h1>				
+			<figure>
+				<?php the_post_thumbnail(large); ?>
+			</figure>
+			<div class="Single-content">
+				<div class="Single-data">
+					<small><?php the_date(); ?></small>
+					<i><small>por</small> <strong><?php the_author(); ?></strong></i>
+				</div> 
+			     <?php the_content(); ?>
+			     <div class="Single-data Single-data--footer">
+ 					<span>Categoría</span>
+ 					<a href="#"><?php the_category(); ?></a>
+				</div>
+				<div class="Single-comments">
+					<?php comments_template(); ?>
+				</div>
+			</div>			
+				
 					
-					<h1 class="Single-title">						
-						<?php the_title(); ?>						
-					</h1>				
-					<figure>
-						<?php the_post_thumbnail(large); ?>
-					</figure>
-					<div class="Single-content">
-						<div class="Single-data">
-							<small><?php the_date(); ?></small>
-							<i><small>por</small> <strong><?php the_author(); ?></strong></i>
-						</div> 
-					     <?php the_content(); ?>
-					     <div class="Single-data Single-data--footer">
-		 					<span>Categoría</span>
-		 					<a href="#"><?php the_category(); ?></a>
-						</div>
-						<div class="Single-comments">
-							<?php comments_template(); ?>
-						</div>
-					</div>			
-						
-							
-				</article>
-				<?php endwhile; ?>
-				<!-- post navigation -->
-				<?php else: ?>
-					<h3>No se encontraron entradas</h3>
-				<!-- no posts found -->
-				<?php endif; ?>
-
-		
-		</main>
+		</article>
+			<?php endwhile; ?>
+			<!-- post navigation -->
+			<?php else: ?>
+				<h3>No se encontraron entradas</h3>
+			<!-- no posts found -->
+			<?php endif; ?>
 		<div class="Sidebar">
 			<?php include TEMPLATEPATH . '/sidebar.php' ?>
 		</div>
-	</div>
+	
+	</main>
+	
 <?php get_footer(); ?>
 
 </body>
